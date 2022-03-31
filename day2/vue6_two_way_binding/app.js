d41ee2e214ffb172c2e1be085f873df3223c716d
jay <flamesunrises@gmail.com>
2022-03-31 16:37:30 +0800

    feat: computed practice

--- day2/vue6_two_way_binding/app.js
+++ day2/vue6_two_way_binding/app.js
@@ -1,11 +1,30 @@
 Vue.createApp({
-    data() { return { issue: "" } },
+    data() {
+        return {
+            issue: "",
+            issue2: ""
+        }
+    },
     methods: {
+
         setIssue(e) {
             this.issue = e.target.value
         },
         resetIssue() {
             this.issue = ""
+        },
+        resetIssue2() {
+            this.issue2 = ""
         }
+    },
+    computed: {
+        formatIssue() {
+            console.log("output issue called, issue=", this.issue)
+            if (this.issue === "") {
+                return ""
+            }
+            // return `[!!]${this.issue}`
+            return this.issue
+        },
     }
 }).mount("#app")

48759485633a1f717d784bdfdb801766f4c22119
jay <flamesunrises@gmail.com>
2022-03-31 15:23:29 +0800

    feat: urgent practice

--- day2/vue5_callback_vue3/app.js
+++ day2/vue5_callback_vue3/app.js
@@ -2,16 +2,31 @@ const app = Vue.createApp({
     data() {
         return {
             counter1: 0,
-            todo: ''
+            todo: '',
+            urgent: "",
+            checkedUrgent: ""
         }
     },
     methods: {
+        commitUrgent() {
+            this.checkedUrgent = this.urgent;
+        },
+        setUrgent(event) {
+            this.urgent = event.target.value
+        },
+        submitForm() {
+            alert("already submitted")
+        },
+        submitForm2(event) {
+            event.preventDefault()
+            alert("already submit form2")
+        },
         increase1() { this.counter1 = this.counter1 + 1 },
         decrease1() { this.counter1 = this.counter1 - 1 },
         increase2(step) { this.counter1 += step },
         decrease2(step) { this.counter1 -= step },
-        setToDo(event, greeting) {
-            this.todo = `${greeting} ${event.target.value}`
+        setToDo(e, greeting) {
+            this.todo = `${greeting} ${e.target.value}`
         }
     },
 })

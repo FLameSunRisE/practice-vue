fdc6ad3d732562143303c9cbbe80718a498e119b
jay <flamesunrises@gmail.com>
2022-03-31 14:05:26 +0800

    feat: method params test

--- day2/vue5_callback_vue3/app.js
+++ day2/vue5_callback_vue3/app.js
@@ -6,7 +6,9 @@ const app = Vue.createApp({
     },
     methods: {
         increase1() { this.counter1 = this.counter1 + 1 },
-        decrease1() { this.counter1 = this.counter1 - 1 }
+        decrease1() { this.counter1 = this.counter1 - 1 },
+        increase2(step) { this.counter1 += step },
+        decrease2(step) { this.counter1 -= step }
     },
 })
 app.mount("#app")

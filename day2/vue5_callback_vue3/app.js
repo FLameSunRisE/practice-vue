6b2271e4ec4cdb926cf03ec043683e1daadca958
jay <flamesunrises@gmail.com>
2022-03-31 14:15:40 +0800

    feat: add htmlevent practice

--- day2/vue5_callback_vue3/app.js
+++ day2/vue5_callback_vue3/app.js
@@ -1,14 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter1: 0
+            counter1: 0,
+            todo: ''
         }
     },
     methods: {
         increase1() { this.counter1 = this.counter1 + 1 },
         decrease1() { this.counter1 = this.counter1 - 1 },
         increase2(step) { this.counter1 += step },
-        decrease2(step) { this.counter1 -= step }
+        decrease2(step) { this.counter1 -= step },
+        setToDo(event) {
+            this.todo = event.target.value
+        }
     },
 })
 app.mount("#app")

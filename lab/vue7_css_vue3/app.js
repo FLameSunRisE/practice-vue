7fd6dbe3cfc844a22c5ea0364175e2a668617869
jay <flamesunrises@gmail.com>
2022-04-07 11:09:19 +0800

    feat: dynamic change css 2

--- lab/vue7_css_vue3/app.js
+++ lab/vue7_css_vue3/app.js
@@ -1,21 +1,32 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      div1Selected: false,
-      div2Selected: false,
-      div3Selected: false,
-    };
-  },
-  methods: {
-    divSelected(id) {
-      if (id === 1) {
-        this.div1Selected = true;
-      } else if (id === 2) {
-        this.div2Selected = true;
-      } else if (id === 3) {
-        this.div3Selected = true;
-      }
+    data() {
+        return {
+            div1Selected: false,
+            div2Selected: false,
+            div3Selected: false
+        }
     },
-  },
-});
-app.mount("#app");
+    methods: {
+        divSelected(id) {
+            if (id === 1) {
+                this.div1Selected = !this.div1Selected;
+            } else if (id === 2) {
+                this.div2Selected = !this.div2Selected;
+            } else if (id === 3) {
+                this.div3Selected = !this.div3Selected;
+            }
+        }
+    },
+    computed: {
+        div1Classes() {
+            return { isSelected: this.div1Selected }
+        },
+        div2Classes() {
+            return { isSelected: this.div2Selected }
+        },
+        div3Classes() {
+            return { isSelected: this.div3Selected }
+        }
+    }
+})
+app.mount("#app")

0459ed3beb93fb8d4ece6234036bef6792e50688
jay <flamesunrises@gmail.com>
2022-04-08 09:42:27 +0800

    feat: prox example add local variable

--- lab/vue10_component2_vue3/src/components/CourseIntro.vue
+++ lab/vue10_component2_vue3/src/components/CourseIntro.vue
@@ -1,8 +1,8 @@
 <template>
   <li>
-    <h2>{{ id }}-{{ current }}</h2>
+    <h2>{{ id }}-{{ isCurrent }}</h2>
     <button @click="toggleCourseDetail">Show detail</button>
-    <button @click="toggleCurrent">Set current</button>
+    <button @click="toggleCurrent">toggle current class</button>
     <ul v-if="detailsVisible">
       <li>{{ name }}</li>
       <li>{{ duration }}</li>
@@ -12,10 +12,10 @@
 <script>
 export default {
   props: ["id", "name", "duration", "current"],
-
   data() {
     return {
       detailsVisible: true,
+      isCurrent: this.current,
     };
   },
   methods: {
@@ -23,7 +23,8 @@ export default {
       this.detailsVisible = !this.detailsVisible;
     },
     toggleCurrent() {
-      this.current = true;
+      this.isCurrent = !this.isCurrent;
+      //this.current = !this.current;
     },
   },
 };

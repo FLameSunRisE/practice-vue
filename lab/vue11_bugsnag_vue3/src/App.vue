875ccb4c8db2ff78e93ada2b69c7ea1193b98a59
jay <flamesunrises@gmail.com>
2022-04-08 16:08:50 +0800

    feat: composition api demo

--- lab/vue11_bugsnag_vue3/src/App.vue
+++ lab/vue11_bugsnag_vue3/src/App.vue
@@ -2,14 +2,23 @@
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button v-on:click="recordBug">trigger a bug</button>
   </div>
 </template>
+
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Bugsnag from '@bugsnag/js'
 export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  method: {
+    recordBug() {
+      console.log("oops, something wrong happen");
+      Bugsnag.notify(new Error("something bad happen in Helloworld.vue...."));
+    },
   },
 };
 </script>
---
+++ lab/vue12_composition_vue3/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h3>{{ userName }}</h3>
+    <h4>{{ age }}</h4>
+    <br />
+    <h1>{{ user }}</h1>
+    <h3>{{user2Name}}</h3>
+    <h4>{{user2Age}}</h4>
+  </section>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  //data(){ return{userName:"Mark Ho"}}
+  setup() {
+    const userNameRef = ref("Mark Ho");
+    const ageRef = ref(45);
+    const userRef = ref({ userName: "James Liu", age: 36 });
+    setTimeout(function () {
+      userNameRef.value = "Meng-Hang Ho";
+      ageRef.value += 1;
+      userRef.value.userName = "Po-Wei Liu"
+      userRef.value.age += 1;
+    }, 5000);
+    return { userName: userNameRef, age: ageRef, user: userRef,
+    user2Name:userRef.value.userName, user2Age:userRef.value.age };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
